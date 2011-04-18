e65c500594517c2f08db7ed83f0bface502de2b9
jackywang <jacky.wang@autodesk.com>
2011-04-18 17:49:36 +0800

    initialize with authenticaton

---
+++ config/routes.rb
@@ -0,0 +1,28 @@
+EWebReputation::Application.routes.draw do
+
+  # eWebReputation
+  match "/signin" => "services#signin"
+  match "/signout" => "services#signout"
+
+  match '/auth/:service/callback' => 'services#create' 
+  match '/auth/failure' => 'services#failure'
+
+  resources :services, :only => [:index, :create, :destroy] do
+    collection do
+      get 'signin'
+      get 'signout'
+      get 'signup'
+      post 'newaccount'
+      get 'failure'
+    end
+  end
+
+  # used for the demo application only
+  resources :users, :only => [:index] do
+    collection do
+      get 'test'
+    end
+  end
+   
+  root :to => "users#index"
+end
